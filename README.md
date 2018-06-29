daaf7e90c37c909267b43b7df2d285d03b7689c2
caiyaming <caiyaming@me.com>
2018-06-29 08:16:28 +0800

    rest-hapi init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # demo-service-hapi
+
+# node version update
+please update node to 8.x
+
+otherwise once use rest-hapi, it would throw error 
+
+/Documents/ionic/demo-service-hapi/node_modules/inert/lib/fs.js:117
+    exports[method] = Util.promisify(NodeFs[method]);
+                           ^
+TypeError: Util.promisify is not a function
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const Hapi = require("hapi");
 const Joi = require("joi");
+let mongoose = require('mongoose');
+let RestHapi = require('rest-hapi');
+ 
 
-const server = new Hapi.Server({  
+/*
+const server = new Hapi.Server({
     host: 'localhost',
     port: 3000
-  });
+});
 
-//server.connection({ "host": "localhost", "port": 3000 });
+// let config = {
+//               appTitle: "demo-service-hapi using rest-hapi",
+//           };
 
-// server.route({
-//     method: "GET",
-//     path: "/",
-//     handler: (request, response) => {
-//         response("Hello World");
+// server.register({
+//     plugin: RestHapi,
+//     options: {
+//         mongoose,
+//         config
 //     }
 // });
 
@@ -26,8 +32,44 @@ server.route({
 });
 
 server.start(error => {
-    if(error) {
+    if (error) {
         throw error;
     }
     console.log("Listening at " + server.info.uri);
 });
+*/
+
+async function api(){
+    try {
+      let server = Hapi.Server({ port: 3000 });
+
+      let config = {
+          appTitle: "demo-service-rest-hapi",
+          version: '1.0.0',
+          authStrategy: false ,
+      };
+     /**
+     * Mongo settings
+     * - config.mongo.URI = 'mongodb://localhost/rest_hapi'; (local db, default)
+     */
+    //config.mongo.URI = 'mongodb://localhost/rest_hapi'
+
+      await server.register({
+        plugin: RestHapi,
+        options: {
+          mongoose,
+          config
+        }
+      });
+
+      await server.start();
+
+      console.log("Server ready", server.info);
+
+      return server;
+    } catch (err) {
+      console.log("Error starting server:", err);
+    }
+  }
+
+  module.exports = api();
